3c387059ab1e24d09e01a47be0b35dde0e7e348a
epombo <epombo@goodgamestudios.com>
2017-02-24 17:37:35 +0100

    [OpenGL] Drawing a triangle pt3 - using vec4 instead vect4

--- GLRendering/Main.cpp
+++ GLRendering/Main.cpp
@@ -28,7 +28,7 @@ const GLchar* vertexShaderSource = "#version 330 core\n"
 "}\0";
 
 const GLchar* fragmentShaderSource = "#version 330 core \n"
-"out vect4 color;\n"
+"out vec4 color;\n"
 "void main()\n"
 "{\n"
 "color = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
